87c6eb39b4f19e85ba73412b5c6076b18d6b93ce
S.Kosovichev <sergksi@mail.ru>
2022-11-05 19:30:25 +0300

    Update Test for Selenide

---
+++ src/test/java/OrderCardTest.java
@@ -0,0 +1,20 @@
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class OrderCardTest {
+
+    @Test
+    void orderCardTest() {
+        open("http://localhost:9999");
+        $("[data-test-id=\"name\"] input").sendKeys("Сергей");
+        $("[data-test-id=\"phone\"] input").sendKeys("+79103258888");
+        $("[data-test-id=\"agreement\"] span").click();
+        $("button").click();
+
+        $("[data-test-id=\"order-success\"]").shouldHave(Condition.text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+}

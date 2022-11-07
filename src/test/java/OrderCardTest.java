1c05959685cb9827835632ba5e234ad7f1f50b14
S.Kosovichev <sergksi@mail.ru>
2022-11-07 12:23:05 +0300

    update test

--- src/test/java/OrderCardTest.java
+++ src/test/java/OrderCardTest.java
@@ -16,5 +16,27 @@ public class OrderCardTest {
 
         $("[data-test-id=\"order-success\"]").shouldHave(Condition.text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+ @Test
+    void orderCardTestAllDataNameFam() {
+        open("http://localhost:9999");
+        $("[data-test-id=\"name\"] input").sendKeys("Сергей Иванов");
+        $("[data-test-id=\"phone\"] input").sendKeys("+79103258833");
+        $("[data-test-id=\"agreement\"] span").click();
+        $("button").click();
+
+        $("[data-test-id=\"order-success\"]").shouldHave(Condition.text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void orderCardTestAllDataFamName() {
+        open("http://localhost:9999");
+        $("[data-test-id=\"name\"] input").sendKeys("Иванов Сергей");
+        $("[data-test-id=\"phone\"] input").sendKeys("+79103258822");
+        $("[data-test-id=\"agreement\"] span").click();
+        $("button").click();
+
+        $("[data-test-id=\"order-success\"]").shouldHave(Condition.text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
 
 }
